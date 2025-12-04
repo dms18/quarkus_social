83c1377c87ae2f7e28186675d50206096b789e1c
dms18 <diegomarquessoare@gmail.com>
2025-12-03 23:22:44 -0300

    commit sem username

--- src/main/java/io/github/diegoms/quarkusSocial/domain/model/User.java
+++ src/main/java/io/github/diegoms/quarkusSocial/domain/model/User.java
@@ -11,9 +11,6 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name")
-    private String name;
-
     @Column(name = "age")
     private Integer age;
 
